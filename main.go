ff63f46b1713f50c2fb41237a40866b09e04c663
omerfruk <omer.fruk3547@gmail.com>
2021-04-14 16:03:21 +0300

    verilerin cekilmesi yapildi

--- main.go
+++ main.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"encoding/json"
-	_ "encoding/json"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/djimenez/iconv-go"
+	_ "github.com/djimenez/iconv-go"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 )
 
-func SayfaninMetniniGetir(konuId int, sayfaNu int) Hadis {
+func KonulariGettir(konuId int) map[int]int {
+	url := fmt.Sprintf("http://islamilimleri.com/Ktphn/Kitablar/05/001/Turkce/%02d/000.htm", konuId)
 
-	url := fmt.Sprintf("http://islamilimleri.com/Ktphn/Kitablar/05/001/Turkce/%02d/%03d.htm", konuId, sayfaNu)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	utfBody, err := iconv.NewReader(resp.Body, charset, "utf-8")
+	if err != nil {
+		// handler error
+	}
+
+	doc, err := goquery.NewDocumentFromReader(utfBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := make(map[int]int)
+	doc.Find("select[name=CD39] option").Each(func(i int, selection *goquery.Selection) {
+		if selection.Text() != "BAB" {
+
+			deger, err := strconv.Atoi(selection.Text())
+			if err != nil {
+				fmt.Println(err)
+			}
+			m[i] = deger
+		}
+	})
+	return m
+
+}
+
+func SayfaninMetniniGetir(konuId int) []Hadis {
+
+	url := fmt.Sprintf("http://islamilimleri.com/Ktphn/Kitablar/05/001/Turkce/%02d/000.htm", konuId)
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -29,7 +67,7 @@ func SayfaninMetniniGetir(konuId int, sayfaNu int) Hadis {
 	if err != nil {
 		log.Fatal(err)
 	}
-	icerik, err := doc.Find("#icerik").Html()
+	icerik := doc.Find("#icerik").Text()
 
 	secimiBul := func(name string) string {
 		secim := ""
@@ -40,22 +78,25 @@ func SayfaninMetniniGetir(konuId int, sayfaNu int) Hadis {
 		})
 		return secim
 	}
+	var Hadisler []Hadis
 	konu := secimiBul("CD71")
-	strhadis := secimiBul("CD39")
-	no, _ := strconv.Atoi(strhadis)
-
-	return Hadis{
-		Konu:   konu,
-		Numara: no,
-		Metin:  icerik,
+	konular := KonulariGettir(konuId)
+	for _, deger := range konular {
+		Hadisler = append(Hadisler, Hadis{
+			Konu:   konu,
+			Numara: deger,
+			Metin:  icerik,
+		})
 	}
+	return Hadisler
 }
 
 func main() {
 	hadisler := make([]Hadis, 0)
-	for hno := 1; hno < 10; hno++ {
-		hadis := SayfaninMetniniGetir(2, hno)
-		hadisler = append(hadisler, hadis)
+	KonulariGettir(1)
+	for hno := 1; hno < 98; hno++ {
+		Thadis := SayfaninMetniniGetir(hno)
+		hadisler = append(hadisler, Thadis...)
 	}
 	if data, err := json.MarshalIndent(hadisler, " ", " "); err != nil {
 		log.Fatal(err)
