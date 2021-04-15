2877d4ca5172fca8730a703e2507da474b2dc543
omerfruk <omer.fruk3547@gmail.com>
2021-04-15 14:24:54 +0300

    html tagları eklenmesi icin struct yapsısı değişti

--- Hadis.go
+++ Hadis.go
@@ -1,7 +1,8 @@
 package main
 
 type Hadis struct {
-	Konu   string `json:"konu"`
-	Numara int    `json:"numara"`
-	Metin  string `json:"metin"`
+	Konu    string   `json:"konu"`
+	Numara  int      `json:"numara"`
+	Metin   []string `json:"metin"`
+	HtmlTag []string `json:"html_tag"`
 }
